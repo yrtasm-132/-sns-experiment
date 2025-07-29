0bca251cf0b99b1be7f2c9652f8192f85896e36b
yrtasm-132 <yrtasm132@gmail.com>
2025-07-29 21:23:06 +0900

    Add files via upload

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
   // 学籍番号のバリデーション（AABAAAA：数字2 + 英大文字1 + 数字4）
   const regex = /^[0-9]{2}[A-Z][0-9]{4}$/;
   // 仮ログインチェック（本番環境では削除・変更）
-   if (regex.test(studentNumber)) {
+   if (regex.test(student_number)) {
     window.location.href = 'main.html';
   }
 });
